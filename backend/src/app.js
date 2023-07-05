31ba7fdb3eae16c0d74ede8457c4486715a8a788
Bence Simon <65396999+ImLumi@users.noreply.github.com>
2023-07-05 18:59:29 +0200

    fix(app): add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import errorHandler from './middlewares/error-handler-middleware';
 import apiRoute from './routes/api-route';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(apiRoute);
